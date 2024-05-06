341dd230ab8fceb411797ea1ec3b9f697bd797d8
Aryan Gupta <Aryangupta.x01@gmail.com>
2024-05-06 16:42:18 +0530

    huge

---
+++ Backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const connectDB = async() => {
+    await mongoose.connect('mongodb+srv://aryanguptax01:aryangupta01@cluster0.tcl3crf.mongodb.net/tomato')
+    .then(() => {
+        console.log("DB Connected");
+    })
+}
+
