0514bfc10e60758c9010dd42035f7a9fb3fc3e93
Aryan Gupta <Aryangupta.x01@gmail.com>
2024-05-09 16:06:46 +0530

    hh

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -9,7 +9,7 @@ const StoreContextprovider = (props) => {
     const [cartItems, setCartItems] = useState({});
 
     //backend Url.
-    const url = "http://localhost:4000";
+    const url = "https://tomato-food-delivery-5t39.onrender.com";
     const [token,setToken] = useState("");
     const [food_list,setFoodList] = useState([]);
 
